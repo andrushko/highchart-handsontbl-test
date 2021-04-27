078b5ea1ba855aa7d89db21d2e92e831bf7b54c2
ANDRUSHKOPC\Andrushko <andrushko.oleh94@gmail.com>
2021-04-27 11:27:38 +0300

    Test

--- app/app.js
+++ app/app.js
@@ -1,4 +1,40 @@
 angular.module('DemoApp', [])
-.controller('DemoController', ['$scope', function($scope) {
+.controller('DemoController', ['$scope', 'api', 'chart', 'datatable', function($scope, api, chart, datatable) {
     $scope.message = "Hello World";
+    
+    $scope.init = function() {
+        api.getData()
+        .then(response => {
+            $scope.data = response.data;
+            $scope.headers = response.headers;
+
+            $scope.datatable = datatable.config(response.headers, 
+                response.data,
+                {
+                    beforeChange: function(arg1, arg2, arg3){
+                        $scope.dataTableUpdated(arg1);
+                    }
+                });
+            $scope.chart = chart.config(response.headers, response.data);
+        });
+    };
+
+    $scope.dataTableUpdated = function(updated) {
+        let doUpdate = false;
+        for (let i = 0; i < updated.length; i++) {
+            let newval = +updated[i][3];
+            let oldval = updated[i][2];
+            
+            if(oldval != newval) {
+                let ix = updated[i][0];
+                let jx = updated[i][1];
+                $scope.data[ix][jx] = newval;
+                doUpdate = true;
+            }
+        }
+        if(doUpdate){
+            let newSeries = chart.buildSeries($scope.data);
+            $scope.$broadcast("dataSeriesUpdated", newSeries); 
+        }
+    }
 }]);
